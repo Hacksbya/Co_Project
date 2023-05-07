aedf1211c1a37b380865f3b9adda178ae3af29e8
Harshit Gautam <119068258+Hacksbya@users.noreply.github.com>
2023-05-07 13:23:02 +0530

    Add files via upload

---
+++ Proj_rough1.py
@@ -0,0 +1,33 @@
+f=open("co.txt")
+assemb_prg=[]
+for i in f.readlines():
+    words=i.strip().split()
+    assemb_prg.append(words)
+print(assemb_prg)
+for i in assemb_prg:
+    # if i==[]:
+    #     assemb_prg.remove(i)
+    for j in i:
+        if "$" in j:
+            i[2]=j[1:]
+variable={}
+
+
+for inst in assemb_prg:
+    if inst[0]=='var':
+        if "var" in variable:
+            variable["var"].append(inst[1])
+        else:
+            variable["var"]=[inst[1]]
+
+for i,j in variable.items():
+    for var in range(len(j)):
+        assemb_prg[var]=None
+i=0
+while(i<len(assemb_prg)):
+    if None in assemb_prg:
+        assemb_prg.remove(None)
+    i+=1
+
+print(variable,"\n",assemb_prg)
+
