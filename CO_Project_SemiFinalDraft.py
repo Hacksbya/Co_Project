f96bba978956b9ae4cee312a409322ee2654fc19
Harshit Gautam <119068258+Hacksbya@users.noreply.github.com>
2023-05-15 17:35:29 +0530

    Update CO_Project_FinalDraft.py

--- CO_Project_SemiFinalDraft.py
+++ CO_Project_SemiFinalDraft.py
@@ -14,6 +14,7 @@ def Fn_A(i):
 
 def Fn_B(i):
     s=(Type_B[i[0]])
+    s+='0'
     s+=register[i[1]]
     p=i[2]
     p=p[1:]
@@ -50,13 +51,13 @@ def Fn_F(i):
 
 
 #main program
-f=open("E:\Projects\CO_CourseProject\co.txt.txt")
+f=open("co.txt")
 assemb_prg=[]
 for i in f.readlines():
     words=i.strip().split()
     assemb_prg.append(words)
 
-#checking for the error of type_I
+#checking for the error of type_i
 if assemb_prg[-1][-1]!="hlt":
     error_i=1
     error_count+=1
@@ -77,14 +78,28 @@ for inst in assemb_prg:
             variable["var"].append(inst[1])
         else:
             variable["var"]=[inst[1]]
+            assemb_prg
+    else:
+        break
+#checking for error of type G
+
+
+# if len(variable) == 0:
+#     error_g=1
+#     error_count+=1
+#     print("Variables not declared at the beginning")
+#     exit()
+
 #var is a 2-D list having variables inside the second list
 for j in variable.values():
     var.append(j)
+if len(variable)==0:
+    pass
+else:
+    variable_dict = {var: i for i, var in enumerate(variable['var'])}
+    variable['var'] = variable_dict
 
-variable_dict = {var: i for i, var in enumerate(variable['var'])}
-variable['var'] = variable_dict
-
-#coverting all the variables in the list to None
+#converting all the variables in the list to None
 for i,j in variable.items():
     for var in range(len(j)):
         assemb_prg[var]=None
@@ -95,6 +110,13 @@ while(i<len(assemb_prg)):
     if None in assemb_prg:
         assemb_prg.remove(None)
     i+=1
+
+#checking for error of type G
+
+for i in assemb_prg:
+    if i[0]=='var':
+        error_g=1
+        print("Variables not declared at the beginning")
 
 #assigning the addrersses to the variables
 for k in variable.values():
@@ -108,19 +130,26 @@ c=0
 #+ checking for the error of type_H
 error_h=1
 for i in assemb_prg:
+    for j in i:
+        if j=='hlt':
+            error_h=0
+            break
     if ':' in i[0]:
-        error_h=0
-        error_count+=1
         label[i[0]]=format(assemb_prg.index(i),'07b')
         i.remove(i[0])
+
+if(error_h==1):
+    error_count+=1
 
 #assigning the address to the hlt statement
 new_label = {}
 for k, v in label.items():
     new_label[k.rstrip(':')] = v
 
-
-#print(variable,"\n",assemb_prg)
+#removing label
+for i in assemb_prg:
+    if i==[]:
+        assemb_prg.remove(i)
 
 Type_A={"add":"00000","sub":"00001","mul":"00110","xor":"01010","or":"01011","and":"01100"}
 Type_B={"mov":"00010","rs":"01000","ls":"01001"}
@@ -131,18 +160,112 @@ Type_F={"hlt":"11010"}
 
 register={"R0":"000","R1":"001","R2":"010","R3":"011","R4":"100","R5":"101","R6":"110","FLAGS":"111"}
 
+
+
 #checking for the error of type E
 for i in assemb_prg:
     if i[0] in Type_B:
         temp_e=i[2][1:]
-        if int(temp_e)>=128:
+        if temp_e.isdigit()==False:
+            print(f"No variable named {i[2]}")
+        elif int(temp_e)>=128:
             error_e=1
             error_count+=1
 
+#checking for error of type A
+def detect_typo(assemb_prg):          
+    inst_name = ["add" , "sub" , "mul" , "xor" , "or" , "and" , "mov" , "div" , "not" , "cmp" , "ld" , "st" , "jmp" , "jlt" , "jgt" , "je" , "hlt"]
+    reg_name = ["R0" , "R1" ,"R2" , "R3" , "R4" , "R5" , "R6" , "FLAGS"]
+    isError = False
+    for i in assemb_prg:           #i -> instruction
+        if i[0] not in inst_name:
+            isError = True
+        
+        for r in i[1:]:  
+                if 'var' not in r:
+                    if 'label' not in r:
+                            if '$' not in r:
+                                if "R" in r:
+                                    if r not in reg_name:
+                                        isError = True   
+               
+    return isError 
+
+if detect_typo(assemb_prg) == True:
+    error_a = 1
+    error_count += 1
+
+#checking for error of type B
+
+def detect_undefined_var(assemb_prg):
+    isError = False
+    for i in assemb_prg:
+        if i[0]=='st' or i[0]== 'ld':
+            if i[2] not in variable['var'].keys():
+                isError=True
+                       #print the error message           
+    return isError  
+
+if detect_undefined_var(assemb_prg) == True:
+    error_b = 1
+    error_count += 1
+
+#checking for error of type C
+def detect_undefined_label(assemb_prg):             
+    labels = set(new_label.keys())
+    isError = False
+    for i in assemb_prg:
+        if i[0] in ["jmp", "jlt", "jgt", "je"]:
+            if i[1] not in labels:
+                isError = True
+            elif 'label' in label:
+                isError = True
+    return isError
+                
+if detect_undefined_label(assemb_prg) == True:
+    error_c = 1
+    error_count += 1 
+
+
+#checking for error of type D``
+def detect_illegal_flags(assemb_prg):          
+    isIllegal = False
+
+    for i in assemb_prg:
+        if i[0] in ["add", "sub", "mul", "xor", "or", "and", "div", "not", "cmp"]:
+            for j in i[1:]:
+                if j == "FLAGS":
+                    isIllegal = True
+    
+    return isIllegal
+
+if detect_illegal_flags(assemb_prg) == True:
+    error_d = 1
+    error_count += 1
+
+#checking of error of type_F
+
+def Error_f(assemb_prg):
+    isError=False
+    for i in assemb_prg:
+        if i[0] in ["jmp", "jlt", "jgt", "je"]:
+            if 'label' not in i[1]:
+                isError=True
+
+        if i[0] in ["add" , "sub" , "mul" , "xor" , "or" , "and" , "mov" , "div" , "not" , "cmp" , "ld" , "st" ]:
+            if 'label' in i[1:0]:
+                isError=True
+    
+    return isError
+
+
+if Error_f(assemb_prg) == True:
+    error_f=1
+    error_count+=1
+
+
 #handeling errors because of different flags that could be raised because of possible errors
-if(error_count==1):
-    if(error_a==1):
-        print("Typo in instruction name or register name")
+if(error_count>=1):
     if(error_b==1):
         print("Use of undefined variables")
     if(error_c==1):
@@ -153,16 +276,16 @@ if(error_count==1):
         print("Illegal Immediate values (more than 7 bits)")
     if(error_f==1):
         print("Misuse of labels as variables or vice-versa")
-    if(error_g==1):
-        print("Variables not declared at the beginning")
     if(error_h==1):
         print("Missing hlt instruction")
     if(error_i==1):
         print("hlt not being used as the last instruction")
-        
-elif(error_count>1):
-    print("General Syntax Error")
-
+    elif (error_a==1):
+        print("Typo in instruction name or register name")
+    exit()        
+# elif(error_count>1):
+#     print("General Syntax Error")
+#     exit()
 
 
 #printing the machine code by calling different functions
